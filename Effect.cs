ede869af02ef380b007c8e7505efb6405dae3501
duswn553 <duswn553@7fb88d5d-4325-0510-a808-c849d00dc437>
2015-11-26 02:43:22 +0000

    먼지 이펙트 추가

---
+++ Effect.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Effect : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Invoke("AutoDestroy", 3);
+	}
+
+    void AutoDestroy()
+    {
+        Destroy(this.gameObject);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
