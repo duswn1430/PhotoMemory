a3282e5379f7f9c3358ddc12b9ff39e9e87552a0
duswn553 <duswn553@7fb88d5d-4325-0510-a808-c849d00dc437>
2015-12-01 07:45:33 +0000

    AudioSource 추가

--- Sound.cs
+++ Sound.cs
@@ -5,7 +5,7 @@ public class Sound : MonoBehaviour
 {
     public static Sound _Instance = null;
 
-    public AudioSource _audio = null;
+    public AudioSource[] _audio = null;
 
     public AudioClip[] _Clips;
 
@@ -14,16 +14,34 @@ public class Sound : MonoBehaviour
         _Instance = this;
     }
 
+    void Start()
+    {
+        _audio = transform.GetComponents<AudioSource>();
+    }
+
     public void Play(string clip)
     {
         for (int i = 0; i < _Clips.Length; ++i)
         {
             if (_Clips[i].name.Equals(clip))
             {
-                _audio.clip = _Clips[i];
-                _audio.Play();
+                SoundPlay(_Clips[i]);
             }
 
+        }
+    }
+
+    void SoundPlay(AudioClip clip)
+    {
+        for (int i = 0; i < _audio.Length; ++i)
+        {
+            if (_audio[i].isPlaying)
+                continue;
+
+            _audio[i].clip = clip;
+            _audio[i].Play();
+
+            break;
         }
     }
 }
