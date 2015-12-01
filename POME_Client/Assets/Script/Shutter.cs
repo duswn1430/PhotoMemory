4cfb16d794df695428137367a699e76185ace0a6
duswn553 <duswn553@7fb88d5d-4325-0510-a808-c849d00dc437>
2015-12-01 06:32:37 +0000

    패널 추가 (Start, Game, Pause, Result)

--- POME_Client/Assets/Script/Shutter.cs
+++ POME_Client/Assets/Script/Shutter.cs
@@ -5,13 +5,23 @@ public class Shutter : MonoBehaviour
 {
     public Animator _animator = null;
 
+    void Start()
+    {
+        Invoke("Init", 1);
+    }
+
+    void Init()
+    {
+        transform.localPosition = new Vector3(0, 0, -50);
+    }
+
     public void PlayOpen()
     {
-        _animator.Play("FXSemo_Open");
+        _animator.Play("FXSemoWB_Open");
     }
 
     public void PlayClose()
     {
-        _animator.Play("FXSemo_Close");
+        _animator.Play("FXSemoWB_Close");
     }
 }
--- POME_Client/Assets/Script/UIManager.cs
+++ POME_Client/Assets/Script/UIManager.cs
@@ -3,11 +3,18 @@ using System.Collections;
 
 public class UIManager : MonoBehaviour
 {
+    public UIPanel _StartPanel = null;
+    public UIPanel _GamePanel = null;
+    public UIPanel _ResultPanel = null;
+    public UIPanel _PausePanel = null;
 
     // Use this for initialization
     void Start()
     {
-
+        _StartPanel.gameObject.SetActive(true);
+        _GamePanel.gameObject.SetActive(false);
+        _ResultPanel.gameObject.SetActive(false);
+        _PausePanel.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -15,4 +22,45 @@ public class UIManager : MonoBehaviour
     {
 
     }
+
+    public void START()
+    {
+        _StartPanel.gameObject.SetActive(false);
+        _GamePanel.gameObject.SetActive(true);
+    }
+
+    public void HELP()
+    {
+        Debug.Log("????????????");
+    }
+
+    public void PAUSE()
+    {
+        _PausePanel.gameObject.SetActive(true);
+    }
+
+    public void CONTINUE()
+    {
+        _PausePanel.gameObject.SetActive(false);
+    }
+
+    public void MAINMENU()
+    {
+        _StartPanel.gameObject.SetActive(true);
+        _GamePanel.gameObject.SetActive(false);
+        _ResultPanel.gameObject.SetActive(false);
+        _PausePanel.gameObject.SetActive(false);
+    }
+
+    public void RANK()
+    {
+        Debug.Log("????????????");
+    }
+
+    public void SOUND()
+    {
+        Debug.Log("????????????");
+    }
+
+
 }
