112405c7a4afa904a843a3657f0c7a89c1fa759d
duswn553 <duswn553@7fb88d5d-4325-0510-a808-c849d00dc437>
2015-12-01 04:15:08 +0000

    셔터 바꿈

---
+++ Shutter.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shutter : MonoBehaviour
+{
+    public Animator _animator = null;
+
+    public void PlayOpen()
+    {
+        _animator.Play("FXSemo_Open");
+    }
+
+    public void PlayClose()
+    {
+        _animator.Play("FXSemo_Close");
+    }
+}
