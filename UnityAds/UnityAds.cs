982638987fd45b34101ba9e2ed65012ad65a456b
duswn553 <duswn553@7fb88d5d-4325-0510-a808-c849d00dc437>
2015-12-10 12:12:05 +0000

    UnityAds 추가

---
+++ UnityAds/UnityAds.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Advertisements;
+
+public class UnityAds : MonoBehaviour
+{
+    private static UnityAds instance = null;
+    public static UnityAds _Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = (new GameObject("UnityAds")).AddComponent<UnityAds>();
+            }
+            return instance;
+        }
+    }
+
+    public void ShowRewardedAd()
+    {
+        if (Advertisement.IsReady("rewardedVideo"))
+        {
+            var options = new ShowOptions { resultCallback = HandleShowResult };
+            Advertisement.Show("rewardedVideo", options);
+        }
+    }
+
+    private void HandleShowResult(ShowResult result)
+    {
+        switch (result)
+        {
+            case ShowResult.Finished:
+                Debug.Log("The ad was successfully shown.");
+                //
+                // YOUR CODE TO REWARD THE GAMER
+                // Give coins etc.
+                break;
+            case ShowResult.Skipped:
+                Debug.Log("The ad was skipped before reaching the end.");
+                break;
+            case ShowResult.Failed:
+                Debug.LogError("The ad failed to be shown.");
+                break;
+        }
+    }
+}
