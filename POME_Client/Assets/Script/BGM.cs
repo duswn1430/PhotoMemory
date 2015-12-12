ac5a74e2b7c5ba557d4bac6e9d2636592dc771d8
duswn553 <duswn553@7fb88d5d-4325-0510-a808-c849d00dc437>
2015-12-12 11:30:47 +0000

    BGM 적용

---
+++ POME_Client/Assets/Script/BGM.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BGM : MonoBehaviour
+{
+    public static BGM _Instance = null;
+
+    public AudioSource _audio = null;
+
+    public AudioClip[] _Clips;
+
+    enum SORT { FANTASY, RHYTHMICAL };
+
+    SORT _Sort;
+
+    void Awake()
+    {
+        _Instance = this;
+
+        _Sort = SORT.FANTASY;
+    }
+
+    public void Play()
+    {
+        switch (_Sort)
+        {
+            case SORT.FANTASY:
+                _audio.clip = _Clips[0];
+                break;
+            case SORT.RHYTHMICAL:
+                _audio.clip = _Clips[1];
+                break;
+        }
+
+        _audio.Play();
+    }
+
+    public void BGMChange()
+    {
+        _Sort = _Sort == SORT.FANTASY ? SORT.RHYTHMICAL : SORT.FANTASY;
+
+        Play();
+    }
+
+}
